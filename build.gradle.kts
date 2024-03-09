33899823ab5ae144b33ea391bb9f88ee0778bee7
Павлов Иван Иванович <w3dipmail@gmail.com>
2024-03-09 19:36:55 +0300

    hw3 added github actions ci

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+subprojects {
+    repositories {
+        mavenCentral()
+    }
+    group = rootProject.group
+    version = rootProject.version
+}
+
+tasks {
+    create("check") {
+        group = "verification"
+        dependsOn(gradle.includedBuild("crypto-trade").task(":check"))
+    }
+}
